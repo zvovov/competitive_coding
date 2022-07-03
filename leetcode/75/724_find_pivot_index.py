d00c0ed13ee19d565a1f447630bdb35fba6e3bae
Chirag Khatri <zvovov@gmail.com>
2022-07-03 16:12:48 +0530

    New and optimized solution 724 - array

---
+++ leetcode/75/724_find_pivot_index.py
@@ -0,0 +1,38 @@
+# https://leetcode.com/problems/find-pivot-index/
+
+from typing import List
+
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        for i in range(len(nums)):
+            if sum(nums[0:i]) == sum(nums[i+1::]):
+                return i
+        return -1
+        # TLE
+    
+    def pivotIndex_optimized(self, nums: List[int]) -> int:
+        left_sum = 0
+        nums_sum = sum(nums)
+        for i in range(len(nums)):
+            # use left_sum
+            # compute right_sum
+            right_sum = nums_sum - left_sum - nums[i]
+            
+            # check return condition
+            if left_sum == right_sum:
+                return i
+
+            # update left_sum
+            left_sum += nums[i]
+         
+        return -1
+
+solved = Solution()
+# print(solved.pivotIndex([1,7,3,6,5,6]))
+# print(solved.pivotIndex([1,2,3]))
+# print(solved.pivotIndex([2,1,-1]))
+
+print(solved.pivotIndex_optimized([1,7,3,6,5,6]))
+print(solved.pivotIndex_optimized([1,2,3]))
+print(solved.pivotIndex_optimized([2,1,-1]))
