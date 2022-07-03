739f3fdacf99ce1fda14d75c1307e380fcb08337
Chirag Khatri <zvovov@gmail.com>
2022-07-03 16:38:19 +0530

    Reference solution 1480 - array

--- leetcode/75/1480_running_sum_of_1d_array.py
+++ leetcode/75/1480_running_sum_of_1d_array.py
@@ -12,8 +12,22 @@ class Solution:
             output_lst[i] = curr_sum
         
         return output_lst 
+    
+    def runningSum_inplace(self, nums: List[int]) -> List[int]:
+        curr_sum = 0
+
+        for i in range(len(nums)):
+            curr_sum += nums[i]
+            nums[i] = curr_sum
         
+        return nums 
+    
+    def runningSum_inplace_optimized(self, nums: List[int]) -> List[int]:
+        curr_sum = 0
+        return [curr_sum := curr_sum + i for i in nums]
 
 solved = Solution()
 print(solved.runningSum([1,2,4,2,1,2,0]))
+print(solved.runningSum_inplace([1,2,4,2,1,2,0]))
+print(solved.runningSum_inplace_optimized([1,2,4,2,1,2,0]))
 
