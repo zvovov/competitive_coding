903e5dc35d420f8ed1186b997ef9755266a09c96
zvovov <zvovov@gmail.com>
2023-04-21 19:00:57 -0700

    add corrected sol: best time to buy and sell stock

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 1. [Two Sum](leetcode/neetcode_150/two_sum.py): bf, opt; hashmap
 1. [Contains Duplicate](leetcode/neetcode_150/contains_duplicate.py): opt; hashmap
 1. [Valid Anagram](leetcode/neetcode_150/valid_anagram.py): opt; hashmap
+
+### Two Pointers
+**Easy**
+1. [Valid Palindrome](leetcode\neetcode_150\valid_palindrome.py)
+
+### Sliding Window
+**Easy**
+1. [Best Time to Buy And Sell Stock ](leetcode\neetcode_150\best_time_to_buy_and_sell_stock.py): bf, two pointer, kadane
--- leetcode/neetcode_150/best_time_to_buy_and_sell_stock.py
+++ leetcode/neetcode_150/best_time_to_buy_and_sell_stock.py
@@ -16,21 +16,31 @@ class Solution:
         right = 1
         max_profit = 0
         while right < len(prices):
-            # curr_profit = prices[right] - prices[left]
-            max_profit = max(max_profit, prices[right] - prices[left])
-            if prices[right] < prices[left]:
+            currentProfit = prices[right] - prices[left] #our current Profit
+            max_profit = max(currentProfit, max_profit)
+            if prices[left] > prices[right]:
                 left = right
             right += 1
-
         
+        return max_profit
+
+
+    def maxProfit(self, prices: List[int]) -> int:
+        curr_profit = 0
+        max_profit = 0
+        for i in range(1, len(prices)):
+            curr_profit += prices[i] - prices[i-1]
+            curr_profit = max(0, curr_profit)
+            max_profit = max(max_profit, curr_profit)
+
         return max_profit
 
 solved = Solution()
  
 print(solved.maxProfitBruteForce(prices = [7,1,5,3,6,4])) 
-print(solved.maxProfitBruteForce(prices = [7,6,4,3,1]))
+print(solved.maxProfitBruteForce(prices = [7,6,4,3,5,1]))
 print(solved.maxProfitBruteForce(prices = [17,35,51,30,61,84]))
 
 print(solved.maxProfit(prices = [7,1,5,3,6,4])) 
-print(solved.maxProfit(prices = [7,6,4,3,1]))
+print(solved.maxProfit(prices = [7,6,4,3,5,1]))
 print(solved.maxProfit(prices = [17,35,51,30,61,84]))
