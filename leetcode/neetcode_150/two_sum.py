a77e1c6b307a97620a29b1b9a4a595be0866f744
chirag <zvovov@gmail.com>
2025-07-07 23:48:34 -0500

    re solution: opt, E, array hashmap one pass

--- leetcode/neetcode_150/two_sum.py
+++ leetcode/neetcode_150/two_sum.py
@@ -1,5 +1,42 @@
 # https://leetcode.com/problems/two-sum/
 
+
+# Intuition:
+# (Target - current) = the desired number to find in nums
+
+# Approach:
+# Hashmap one pass
+# While building a hashmap, compute the desired number and find it in nums
+
+# Complexity:
+# Time O(n), for building the hashmap and constant lookups to find the desired number
+# Space O(n), for the hashmap
+
+# Example 1:
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+# desired_hm = {
+#     7: 0,   
+#     2: 1,     <--- .get(nums[i]) == 7
+#     ...
+# }
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        desired_hm = {}
+        for i in range(len(nums)):
+            desired = target - nums[i]
+            # print("Checking desired_hm[nums[{}]]: {}".format(i, desired_hm.get(nums[i])))
+            if desired_hm.get(nums[i]) is not None:
+            # if nums[i] in desired_hm:
+                return [desired_hm.get(nums[i]), i]
+            else:
+                desired_hm[desired] = i
+            # print(desired_hm) 
+
 from typing import List
 
 class Solution_Brute_Force:
