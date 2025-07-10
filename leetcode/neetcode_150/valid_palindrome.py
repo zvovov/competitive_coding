79b83ecac2521acebc6c3559aafd4bd42d03a8d5
chirag <zvovov@gmail.com>
2025-07-09 19:08:28 -0500

    another solution: array, E, two pointer

--- leetcode/neetcode_150/valid_palindrome.py
+++ leetcode/neetcode_150/valid_palindrome.py
@@ -1,5 +1,23 @@
 # https://leetcode.com/problems/valid-palindrome/
 
+# Intuition:
+# Clean the string and check if the string is the same forwards and backwards
+
+# Approach:
+# Remove non-alphanumeric characters and convert to lowercase
+# Use isalnum to remove non-alphanumeric characters
+
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        clean_s = ''.join([char.lower() for char in s if char.isalnum()])
+        for left in range(len(clean_s)):
+            right = len(clean_s) - 1 - left # works because s >= 1
+            if clean_s[left] != clean_s[right]:
+                return False
+        return True
+
 import re
 
 class Solution:
