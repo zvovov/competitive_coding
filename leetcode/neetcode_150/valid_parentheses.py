9f42ac523b757c13608840aaeb6885da076222c3
chirag <zvovov@gmail.com>
2025-07-21 20:16:54 -0500

    new solution: stack, E, more readable

--- leetcode/neetcode_150/valid_parentheses.py
+++ leetcode/neetcode_150/valid_parentheses.py
@@ -1,7 +1,7 @@
 # https://leetcode.com/problems/valid-parentheses/
 
 class Solution:
-    def isValid(self, s: str) -> bool:
+    def isValid_(self, s: str) -> bool:
         # requirements
         # 1. Any type of open is allowed after any type of open
         # 2. The latest open must be closed, and in order for remaining currently open
@@ -36,7 +36,42 @@ class Solution:
                     return False
         return len(stack_) == 0
 
-        
+
+# Intuition:
+# Maintain a stack of incoming parenthesis in order
+
+# Approach:
+# Iterate string, if opening parenthesis is found, insert in stack and move to next element
+# If closing parenthesis is found, pop the stack and match, proceed if match, return false otherwise
+# At the end of the string, if stack is empty return true, false otherwise
+
+# Complexity:
+# Time: O(n) for one loop on input string
+# Space: O(n) for stack
+
+from collections import deque
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        parenth_stack = deque()
+        parenth_pair = {
+            ')': '(',
+            '}': '{',
+            ']': '[',
+        }
+        for i in range(len(s)):
+            if s[i] in ['(', '{', '[']:
+                parenth_stack.append(s[i])
+            else:
+                if len(parenth_stack) == 0:
+                    return False
+                if parenth_stack.pop() == parenth_pair[s[i]]:
+                    continue
+                else: 
+                    return False
+        return len(parenth_stack) == 0        
+
+
 
 solved = Solution()
 #print()
